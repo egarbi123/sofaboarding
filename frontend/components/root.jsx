00b6d6330a3ed69c8a3fd84870270b6f58e7fa0b
egarbi123 <egarbi123@gmail.com>
2020-04-29 11:59:52 -0700

    webpack and basic react setup

---
+++ frontend/components/root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>React Root is connected!</div>
+        )
+    }
+}
+
+export default Root;
